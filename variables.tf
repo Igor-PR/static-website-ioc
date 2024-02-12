f2af204d31089eb3bbdb69909b6d3af0ea6acf88
Igor <igor@blueorange.digital>
2024-02-12 13:55:52 +0000

    remove unused variable

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "aws_profile" {
-  type        = string
-  default     = "default"
-  description = "AWS profile for credentials"
-}
-
 variable "domain_name" {
   type        = string
   description = "Domain name to be used as bucket name. Needs to follow S3 bucket naming conventions"
